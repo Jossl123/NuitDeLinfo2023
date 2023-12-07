1e8530a57648bcb5dad6e9b8c6f5abd5ce2450f7
Jossl123 <jossl1.gau@gmail.com>
2023-12-07 21:23:19 +0100

    merge

---
+++ src/public/parameters.php
@@ -0,0 +1,45 @@
+<!-- home.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="./css/phone.css">
+</head>
+<body>
+    <div id="phone_out">
+        <div id="parameters">
+            <img src="./img/go-back.png" alt="go-back" id="go-back">
+            <h1 id="theme">Theme</h1>
+            <div id="Themes">
+                <div class="Themes-block">
+                    <img src="./img/theme1.png" id="theme1" alt="theme1">
+                    <img src="./img/lock.png" alt="lock" class="lock">
+                </div>
+                <div class="Themes-block">
+                    <img src="./img/theme2.png" id="theme2" alt="theme2">
+                </div>
+                <div class="Themes-block">
+                    <img src="./img/theme3.png" id="theme3" alt="theme3">
+                </div>
+                <div class="Themes-block">
+                    <img src="./img/theme4.png" id="theme4" alt="theme4">
+                </div>
+                <div class="Themes-block">
+                    <img src="./img/theme5.png" id="theme5" alt="theme5">
+                </div>
+                <div class="Themes-block">
+                    <img src="./img/theme6.png" id="theme6" alt="theme6">
+                </div>
+                <div class="Themes-block">
+                    <img src="./img/theme7.png" id="theme7" alt="theme7">
+                </div>
+                <div class="Themes-block">
+                    <img src="./img/theme8.png" id="theme8" alt="theme8">
+                </div>
+            </div>
+        </div>        
+    </div>
+</body>
+</html>
