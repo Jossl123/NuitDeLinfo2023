e7452bcad59cf21a40024478bd50c2855dbb122c
vladblais <blaisvladimir@gmail.com>
2023-12-07 22:19:13 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="wrapper">
+        <!--- load a music .mp3 file-->
+        <audio id="audio" src="music.mp3" preload="auto"></audio>
+        <div id="drop-circle-area">
+        </div>
+        <div id="click-area">
+            <div class="circle">circle</div>
+            <div class="circle">circle</div>
+            <div class="circle">circle</div>
+            <div class="circle">circle</div>
+        </div>
+    </div>
+</body>
+</html>
