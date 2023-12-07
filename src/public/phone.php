09890f49092c694eaff41b319d2429c54aade336
Jossl123 <jossl1.gau@gmail.com>
2023-12-07 18:32:21 +0100

    first phone ux

---
+++ src/public/phone.php
@@ -0,0 +1,26 @@
+<!-- home.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="./css/phone.css">
+</head>
+<body>
+    <div id="phone_out">
+        <div id="phone">
+            <div id="messages">
+                <div class="message you">Hey</div>
+                <div class="message">Hey</div>
+                <div class="message you">rgerzzvey</div>
+                <div class="message">Hey</div>
+                <div class="message you">zrthrtherh</div>
+                <div class="message you">rthzrethzh</div>
+                <div class="message">Hey</div>
+            </div>
+            <div id="type_zone"></div>
+        </div>
+    </div>
+</body>
+</html>
