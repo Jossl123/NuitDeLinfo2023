393128db6db5f0e9ded7e54df3b281b65a845d81
Jossl123 <jossl1.gau@gmail.com>
2023-12-07 21:22:20 +0100

    add conversations for different girls

--- src/public/js/msgLogic.js
+++ src/public/js/msgLogic.js
@@ -1,5 +1,3 @@
-
-
 var story = [
     {
         "type": "message",
--- src/public/phone.php
+++ src/public/phone.php
@@ -5,25 +5,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="./css/phone.css">
+    <link rel="stylesheet" href="./css/phone/phone.css">
+    <link rel="stylesheet" href="./css/phone/conversations.css">
+    <link rel="stylesheet" href="./css/phone/discussion.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 </head>
 <body>
     <div id="phone_out">
         <div id="phone">
-            <button id="params">
-                <div></div>
-                <div></div>
-                <div></div>
-            </button>
-            <div id="messages">
+            <div id="conversations" class="panel">
             </div>
-            <div id="type_zone">
-                <input type="text">
-                <button><span class="material-symbols-outlined">send</span></button>
+            <div id="discussion" class="panel">
+                <button id="params">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </button>
+                <div id="messages">
+                </div>
+                <div id="type_zone">
+                    <input type="text">
+                    <button><span class="material-symbols-outlined">send</span></button>
+                </div>
             </div>
         </div>
     </div>
-    <script src="./js/phone.js"></script>
+    <script src="./js/msgLogic.js"></script>
+    <script src="./js/conversations.js"></script>
 </body>
 </html>
