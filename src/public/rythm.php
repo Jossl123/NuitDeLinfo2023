aa04c19c002f83d1586a6848528b270994446f75
Caetano <caetano.godinat@hotmail.com>
2023-12-08 06:40:48 +0100

    working game

--- src/public/rythm.php
+++ src/public/rythm.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/rythm.css">
     <script src="https://cdn.jsdelivr.net/npm/p5@1/lib/p5.min.js"></script>
-    <script src="./music/music.mp3"></script>
+    <script src="./js/p5.sound.js"></script>
     <script src="./js/app.js"></script>
 </head>
 <body>
